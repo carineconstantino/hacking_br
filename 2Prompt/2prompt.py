e78cbf1463a0dfd8504b5b54ad8feb0664757ae2
constantino <53983340+carineconstantino@users.noreply.github.com>
2025-05-19 18:58:21 -0300

    Update 2prompt.py

--- 2Prompt/2prompt.py
+++ 2Prompt/2prompt.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 import openai
 import google.generativeai as genai
 import google.generativeai
-import anthropic
 import gradio as gr
 from gradio import themes
 
@@ -15,7 +14,6 @@ from gradio import themes
 
 load_dotenv(override=True)
 openai_api_key = os.getenv('OPENAI_API_KEY')
-anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
 google_api_key = os.getenv('GOOGLE_API_KEY')
 
 # Print the key prefixes to help with any debugging
@@ -24,11 +22,6 @@ if openai_api_key:
     print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
 else:
     print("OpenAI API Key not set")
-    
-if anthropic_api_key:
-    print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
-else:
-    print("Anthropic API Key not set")
 
 if google_api_key:
     print(f"Google API Key exists and begins {google_api_key[:8]}")
